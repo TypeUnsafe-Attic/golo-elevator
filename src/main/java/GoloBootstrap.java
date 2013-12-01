be73068235fd4bf316f4d261cec3fd4964a28b59
Daniel Petisme <daniel.petisme@gmail.com>
2013-12-01 12:49:24 +0100

    Fixing cloudbees

--- src/main/java/GoloBootstrap.java
+++ src/main/java/GoloBootstrap.java
@@ -5,13 +5,10 @@ import java.lang.invoke.MethodType;
 public class GoloBootstrap {
 
   public static void main(String[] args) throws Throwable {
-    if(args.length == 1) {
-      String goloMainModuleName = args[0];
+      String goloMainModuleName = "elevator.Main";
       Class klass = Class.forName(goloMainModuleName);
       MethodHandle main = MethodHandles.publicLookup().findStatic(klass, "main", MethodType.genericMethodType(1));
       main.invoke(args);
-    }
-    System.err.println("Usage: GoloBootstrap <golo_main_module_name>");
   }
 }
 
